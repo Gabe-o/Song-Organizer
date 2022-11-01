603f46e325b7a268b6b2a6ee393f63cbeaf99434
Gabriel Olivotto <golivott@uwo.ca>
2022-11-01 15:32:33 -0400

    swapped to using npm csv-parser becuase its faster

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const fs = require('fs');
+const csv = require('csv-parser');
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -36,6 +37,22 @@ app.get('/resetDB', (req, res) => {
 // Routes requests for /api/artists
 const artistRouter = express.Router();
 app.use('/api/artists', artistRouter);
+
+artistRouter.get('', (req, res) => {
+    const artistName = req.query.artistName;
+
+    db.query('SELECT * FROM artists WHERE artistName LIKE' + "'%" + artistName + "%';", (err, data) => {
+        if (data === []) {
+            res.status(404).send("Not Found");
+        }
+        else if (err) {
+            res.status(500).send(err);
+        }
+        else {
+            res.send(data);
+        }
+    });
+})
 
 // Querys db for a given artist id and returns 1 result
 artistRouter.get('/:id', (req, res) => {
@@ -152,56 +169,6 @@ app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 });
 
-// Takes a CVS file and converts it to a JSON object
-function csvToJSON(csvFilePath) {
-    let csv = fs.readFileSync(csvFilePath);// reading csv
-
-    // Getting the headers
-    let csvAsArrayByRows = csv.toString().split("\n");
-    let headers = csvAsArrayByRows[0].split(","); // extracting headers
-
-    // Building new csv as a string without the header line
-    let csvAsString = "";
-    for (let i = 1; i < csvAsArrayByRows.length - 1; i++) {
-        csvAsString += (csvAsArrayByRows[i] + "\n");
-    }
-
-    // Creates an array where each element is a row from the CSV file
-    let valueCounter = 0;
-    let inQuotes = false;
-    let value = "";
-    let obj = {};
-    let csvAsJSON = [];
-    for (let ch of csvAsString) {
-        // Toggles in quotes variable whenever quotes are encountered 
-        if (ch === '"') {
-            inQuotes = !inQuotes;
-        }
-        // If we are not in quotes replace the comma with a |
-        if (ch === ',' && !inQuotes) {
-            obj[headers[valueCounter]] = value.trim();
-            value = "";
-            valueCounter++;
-            continue;
-        }
-        // When a newline is encountered and we are on the last value in a row push the current row to the array and start over
-        if (ch === '\n' && valueCounter === headers.length - 1 && !inQuotes) {
-            obj[headers[headers.length - 1]] = value.trim();
-            csvAsJSON.push(obj);
-            obj = {};
-            value = "";
-            valueCounter = 0;
-            continue;
-        }
-        // add the character to the string
-        if (ch !== '"') {
-            value += ch;
-        }
-    }
-
-    return csvAsJSON;
-}
-
 // Builds Genres table from CSV file
 function buildGenresDB() {
     // Deletes the table if one already exists
@@ -232,21 +199,26 @@ function buildGenresDB() {
     );
 
     // Adds genres from CSV file to the db
-    let genres = csvToJSON("data/genres.csv");
-    let i = 0;
-    for (let genre of genres) {
-        db.query("INSERT INTO genres VALUES (" +
-            genre.genre_id + ", " +
-            genre["#tracks"] + ", " +
-            genre.parent + ", " +
-            '"' + genre.title + '"' + ", " +
-            genre.top_level + ");", (err) => {
-                if (err) {
-                    throw err;
-                }
-                console.log("Inserted genre " + genre.genre_id + " into genres table \t" + ((++i) / genres.length) * 100 + " \t%");
-            });
-    }
+    let genres = [];
+    fs.createReadStream('data/genres.csv')
+        .pipe(csv())
+        .on('data', (data) => genres.push(data))
+        .on('end', () => {
+            let i = 0;
+            for (let genre of genres) {
+                db.query("INSERT INTO genres VALUES (" +
+                    genre.genre_id + ", " +
+                    genre["#tracks"] + ", " +
+                    genre.parent + ", " +
+                    '"' + genre.title.replaceAll('"',"") + '"' + ", " +
+                    genre.top_level + ");", (err) => {
+                        if (err) {
+                            throw err;
+                        }
+                        console.log("Inserted genre " + genre.genre_id + " into genres table \t" + ((++i) / genres.length) * 100 + " \t%");
+                    });
+            }
+        });
 }
 
 // Builds Albums table from CSV file
@@ -276,18 +248,23 @@ function buildAlbumsDB() {
     );
 
     // Adds albums from CSV to the db
-    let albums = csvToJSON("data/raw_albums.csv");
-    let i = 0;
-    for (let album of albums) {
-        db.query("INSERT INTO albums VALUES (" +
-            album.album_id + ", " +
-            '"' + album.album_handle.replaceAll('_', ' ') + '"' + ");", (err) => {
-                if (err) {
-                    throw err;
-                }
-                console.log("Inserted album " + album.album_id + " into albums table \t" + ((++i) / albums.length) * 100 + " \t%");
-            });
-    }
+    let albums = [];
+    fs.createReadStream('data/raw_albums.csv')
+        .pipe(csv())
+        .on('data', (data) => albums.push(data))
+        .on('end', () => {
+            let i = 0;
+            for (let album of albums) {
+                db.query("INSERT INTO albums VALUES (" +
+                    album.album_id + ", " +
+                    '"' + album.album_handle.replace(/[_"]/, ' ').trim() + '"' + ");", (err) => {
+                        if (err) {
+                            throw err;
+                        }
+                        console.log("Inserted album " + album.album_id + " into albums table \t" + ((++i) / albums.length) * 100 + " \t%");
+                    });
+            }
+        });
 }
 
 // Builds Artists table from CSV file
@@ -322,23 +299,28 @@ function buildArtistsDB() {
     );
 
     // Adds artists from the CSV file to the db
-    let artists = csvToJSON("data/raw_artists.csv");
-    let i = 0;
-    for (let artist of artists) {
-        db.query("INSERT INTO artists VALUES (" +
-            artist.artist_id + ", " +
-            '"' + artist.artist_handle.replaceAll('_', ' ') + '"' + ", " +
-            '"' + artist.artist_location.split('\n')[0].slice(0, 99) + '"' + ", " +
-            artist.artist_favorites + ", " +
-            '"' + artist.artist_date_created + '"' + ", " +
-            '"' + artist.artist_website + '"' + ", " +
-            '"' + artist.artist_associated_labels + '"' + ");", (err) => {
-                if (err) {
-                    throw err;
-                }
-                console.log("Inserted artist " + artist.artist_id + " into artists table \t" + ((++i) / artists.length) * 100 + " \t%");
-            });
-    }
+    let artists = [];
+    fs.createReadStream('data/raw_artists.csv')
+        .pipe(csv())
+        .on('data', (data) => artists.push(data))
+        .on('end', () => {
+            let i = 0;
+            for (let artist of artists) {
+                db.query("INSERT INTO artists VALUES (" +
+                    artist.artist_id + ", " +
+                    '"' + artist.artist_handle.replace(/[_"]/, ' ').trim() + '"' + ", " +
+                    '"' + artist.artist_location.replaceAll('"',"").split('\n')[0].slice(0, 99) + '"' + ", " +
+                    artist.artist_favorites + ", " +
+                    '"' + artist.artist_date_created.replaceAll('"',"") + '"' + ", " +
+                    '"' + artist.artist_website.replaceAll('"',"") + '"' + ", " +
+                    '"' + artist.artist_associated_labels.replaceAll('"',"") + '"' + ");", (err) => {
+                        if (err) {
+                            throw err;
+                        }
+                        console.log("Inserted artist " + artist.artist_id + " into artists table \t" + ((++i) / artists.length) * 100 + " \t%");
+                    });
+            }
+        });
 }
 
 // Builds Tracks table from CSV file
@@ -364,7 +346,7 @@ function buildTracksDB() {
         "trackDateCreated char(45) DEFAULT NULL,\n" +
         "trackDateRecorded char(45) DEFAULT NULL,\n" +
         "trackDuration char(45) DEFAULT NULL,\n" +
-        "trackGenres char(100) DEFAULT NULL,\n" +
+        "trackGenres text DEFAULT NULL,\n" +
         "trackNumber int DEFAULT NULL,\n" +
         "trackTitle char(200) DEFAULT NULL,\n" +
         "PRIMARY KEY (trackID)\n);\n", (err) => {
@@ -376,24 +358,29 @@ function buildTracksDB() {
     );
 
     // Adds Tracks from CSV file to the db
-    let tracks = csvToJSON("data/raw_tracks.csv");
-    let i = 0;
-    for (let track of tracks) {
-        db.query("INSERT INTO tracks VALUES (" +
-            track.track_id + ", " +
-            ((track.album_id === '') ? "null" : track.album_id) + ", " +
-            track.artist_id + ", " +
-            '"' + track.tags + '"' + ", " +
-            '"' + track.track_date_created + '"' + ", " +
-            '"' + track.track_date_recorded + '"' + ", " +
-            '"' + track.track_duration + '"' + ", " +
-            '"' + track.genres + '"' + ", " +
-            track.track_number + ", " +
-            '"' + track.track_title.replace(/[;\\]/g, "").trim() + '"' + ");", (err) => {
-                if (err) {
-                    throw err;
-                }
-                console.log("Inserted track " + track.track_id + " into tracks table \t" + ((++i) / tracks.length) * 100 + " \t%");
-            });
-    }
+    let tracks = [];
+    fs.createReadStream('data/raw_tracks.csv')
+        .pipe(csv())
+        .on('data', (data) => tracks.push(data))
+        .on('end', () => {
+            let i = 0;
+            for (let track of tracks) {
+                db.query("INSERT INTO tracks VALUES (" +
+                    track.track_id + ", " +
+                    ((track.album_id === '') ? "null" : track.album_id) + ", " +
+                    track.artist_id + ", " +
+                    '"' + track.tags.replaceAll('"',"") + '"' + ", " +
+                    '"' + track.track_date_created.replaceAll('"',"") + '"' + ", " +
+                    '"' + track.track_date_recorded.replaceAll('"',"") + '"' + ", " +
+                    '"' + track.track_duration.replaceAll('"',"") + '"' + ", " +
+                    '"' + track.track_genres.replaceAll('"',"") + '"' + ", " +
+                    track.track_number + ", " +
+                    '"' + track.track_title.replace(/[;\\"]/g, "").trim() + '"' + ");", (err) => {
+                        if (err) {
+                            throw err;
+                        }
+                        console.log("Inserted track " + track.track_id + " into tracks table \t" + ((++i) / tracks.length) * 100 + " \t%");
+                    });
+            }
+        });
 }
