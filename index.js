71bd9f3865c225515fc9d375c742122101df8990
Gabriel Olivotto <golivott@uwo.ca>
2022-11-02 22:26:38 -0400

    Added Backend functionality for saving list of tracks and getting lists

--- index.js
+++ index.js
@@ -242,11 +242,32 @@ listRouter.post('', (req, res) => {
 
     const listName = req.body.listName;
 
-    db.query("INSERT INTO lists (listName) VALUES (?)", [listName], (err, data) => {
+    db.query("INSERT INTO lists VALUES (?,?,?)", [listName, 0, "00:00"], (err, data) => {
         if (err) {
             if (err.code === 'ER_DUP_ENTRY') {
                 res.status(422).json(err.sqlMessage);
+                return;
             }
+            else {
+                res.status(500).json(err);
+                return;
+            }
+        }
+        else {
+            res.json(data);
+            return;
+        }
+    });
+});
+
+// Get a list of list names, number of tracks that are saved in each list and the total play time of each list.
+listRouter.get('', (req, res) => {
+    db.query("SELECT * FROM lists", (err, data) => {
+        if (err) {
+            res.status(500).json(err);
+        }
+        else if (data.length === 0) {
+            res.status(404).json("No Lists Found");
         }
         else {
             res.json(data);
@@ -254,19 +275,140 @@ listRouter.post('', (req, res) => {
     });
 });
 
-// Get a list of list names
-listRouter.get('', (req, res) => {
-    db.query("SELECT * FROM lists", (err, data) => {
-        if (data.length === 0) {
-            res.status(404).json(data);
+// Get list of track ids for a given list
+listRouter.get('/:listName', (req, res) => {
+    // Input Validation
+    const schema = Joi.object({
+        listName: Joi.string().required()
+    });
+    const result = schema.validate(req.params);
+    if (result.error) {
+        res.status(400).json(result.error.details[0].message);
+        return;
+    }
+
+    const listName = req.params.listName;
+
+    db.query("SELECT listName FROM lists WHERE listName=? LIMIT 1", [listName], (err, data) => {
+        if (err) {
+            res.status(500).json(err);
+            return;
         }
-        else if (err) {
-            throw err;
+        // if list doesn't exist
+        else if (data.length === 0) {
+            res.status(404).json("List Not Found");
+            return;
         }
-        else {
-            res.json(data);
+
+        db.query("SELECT trackID FROM listtrackdetails WHERE listName=?", [listName], (err, data) => {
+            if (err) {
+                res.status(500).json(err);
+                return;
+            }
+            else {
+                res.json(data);
+                return;
+            }
+        });
+        
+    });
+});
+
+// Adding list of tracks to a given list
+listRouter.put('/:listName', (req, res) => {
+    // Input Validation
+    const schema = Joi.object({
+        listName: Joi.string().required(),
+    });
+    const result = schema.validate(req.params);
+    if (result.error) {
+        res.status(400).json(result.error.details[0].message);
+        return;
+    }
+
+    const listName = req.params.listName;
+    let tracks = req.query.tracks.split(",").map(id => parseInt(id)).filter(Boolean);
+
+    // Checks if a list exists
+    db.query("SELECT listName FROM lists WHERE listName=?;", [listName], (err, data) => {
+        if (err) {
+            res.status(500).json(err);
+            return;
         }
-    })
+        else if (data.length === 0) {
+            res.status(404).json("List doesn't Exist");
+            return;
+        }
+
+        // Deletes existing data
+        db.query("DELETE FROM listtrackdetails WHERE listName=?;", [listName], (err) => {
+            if (err) {
+                res.status(500).json(err);
+                return;
+            }
+
+            // Checks to see if the user acctually input any tracks
+            if (tracks.length !== 0) {
+                // Checks if the ids given exist in track db and gets ones that do along with there duration
+                db.query("SELECT trackID,trackDuration FROM tracks WHERE trackID IN (?);", [tracks], (err, data) => {
+                    if (err) {
+                        res.status(500).json(err);
+                        return;
+                    }
+
+                    // Filters tracks requested with tracks that acctually exist in db
+                    let foundIDs = data.map(track => track.trackID);
+                    tracks = tracks.filter(track => foundIDs.includes(track));
+
+                    // Adds duration property to list of filtered tracks
+                    tracks = tracks.map(trackID => {
+                        return {
+                            trackID: trackID,
+                            trackDuration: data[foundIDs.indexOf(trackID)].trackDuration
+                        }
+                    });
+
+                    // For total duration calc
+                    let totalDuration = 0;
+                    // Inserts each track
+                    for (let track of tracks) {
+                        db.query("INSERT INTO listtrackdetails VALUES (?,?);", [listName, track.trackID], (err) => {
+                            if (err) {
+                                res.status(500).json(err);
+                                return;
+                            }
+                        });
+
+                        // Calculation for total duration of list
+                        let trackDur = track.trackDuration.split(":");
+                        totalDuration += parseInt(trackDur[0]) * 60 + parseInt(trackDur[1]);
+                    }
+
+                    // Formoting total duration to string for display
+                    totalDuration = `${parseInt(totalDuration / 60)}:${totalDuration % 60}`;
+
+                    // Updating lists db with new info about that list
+                    db.query("UPDATE lists SET numTracks=?,totalDuration=? WHERE listName=?;", [tracks.length, totalDuration, listName], (err) => {
+                        if (err) {
+                            res.status(500).json(err);
+                            return;
+                        }
+                        res.json();
+                    });
+                });
+            }
+            else {
+                // Updating lists db with new info about that list
+                db.query("UPDATE lists SET numTracks=?,totalDuration=? WHERE listName=?;", [0, "00:00", listName], (err) => {
+                    if (err) {
+                        res.status(500).json(err);
+                        return;
+                    }
+                    res.json();
+                });
+            }
+        });
+    });
 });
 
 
